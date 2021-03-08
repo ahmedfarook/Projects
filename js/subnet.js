3b02bb5f5c6e8df05283842c38b6ae9104f637de
Ahmed Farook <ahmedgaziyani@gmail.com>
2021-03-08 14:10:54 +0530

    commit

--- js/subnet.js
+++ js/subnet.js
@@ -1,25 +1,29 @@
 
 //Creating a Function 
-
+function timeout() {
+    setTimeout(function(){ document.getElementById("error").textContent = ""; }, 5000);
+  }
 //Creating an array of an Object
 
 //var subnet = "10.128.94.0/21"
 var table = document.getElementById("myTable");
 table.style.display = "none"
+var count = ["2","4","8","16","32","64","128"]
 var subnetData = {
     cidr: [21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
     ipadd: [2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4]
 } //console.log(subnetData.cidr.length)
 
 //document.write("Hello");
-
+//document.getElementById("SubnetID").value
 document.getElementById("submit").addEventListener('click', function () {
 
-    
     // Reading the input and saving in to a varibale
     var subnet = document.getElementById("SubnetID").value
     console.log(typeof subnet)
-    var subCount = document.getElementById("count").value
+    var subCount = (document.getElementById("count").value)
+    console.log(count.includes(subCount))
+    if (count.includes(subCount)) {
 
     //
     for (var i = 0; i < subnetData.cidr.length; i++) {
@@ -127,8 +131,15 @@ document.getElementById("submit").addEventListener('click', function () {
 
         
     }
+}
     
-    document.getElementById("submit").disabled = true;
+
+    else {
+        document.getElementById("error").textContent ="error : please enter the input value in proper format"
+        timeout()
+    }
+    
+    //document.getElementById("submit").disabled = true;
 
 
       
